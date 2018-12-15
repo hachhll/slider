69543f2fd401ecd9b66bc53f8200b2dc7633320d
Gevorg <hachhll@yandex.ru>
2018-12-15 11:28:11 +0300

    First step of react intensive

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from 'react-bootstrap/lib/Button';
+import Row from 'react-bootstrap/lib/Button';
+import Col from 'react-bootstrap/lib/Button';
+import styled from 'styled-components'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper"> 
+        <SayFullName name="Gevorg" surname="Khachaturyan" link="yandex.ru" />
+        <SayFullName name="Liana" surname="Khachaturyan" link="yandex.ru" />
+        <SayFullName name="Laura" surname="Khachaturyan" link="yandex.ru" />
       </div>
     );
   }
+}
+
+function SayFullName(props) {
+  return(
+        <div>
+      <h1> Мое имя {props.name}, фамилия - {props.surname}</h1>
+      <a href={props.link}> Ссылка на мой профиль </a>
+      </div>
+    )
 }
 
 export default App;
